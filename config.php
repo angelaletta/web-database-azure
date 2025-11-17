8da5124e4a9711c9c4f7483f673eaf93313e6c76
angelaletta <angelaletta17@gmail.com>
2025-11-17 22:40:42 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = getenv('DB_HOST') ?: 'mysql-angel.mysql.database.azure.com';
-$db_user = getenv('DB_USER') ?: 'angelalettaa';
+$db_user = getenv('DB_USER') ?: 'angelalettaa@mysql-angel';
 $db_pass = getenv('DB_PASS') ?: 'Cressencia17';
 $db_name = getenv('DB_NAME') ?: 'db_mahasiswa';
 $db_port = 3306;
@@ -10,3 +10,4 @@ $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
+
